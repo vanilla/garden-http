a57f14c4d17419b9481a5b910d9fd2dbcb97793f
Todd Burry <todd@vanillaforums.com>
2015-02-04 16:28:44 -0500

    Add more HttpMessage tests.

--- tests/HttpMessageTest.php
+++ tests/HttpMessageTest.php
@@ -16,38 +16,65 @@ class HttpMessageTest extends \PHPUnit_Framework_TestCase {
      * Test basic case-insensitive header access.
      */
     public function testBasicHeaderAccess() {
-        $message = new HttpRequest();
+        $msg = new HttpRequest();
 
-        $message->setHeader('foo', 'bar');
-        $this->assertSame('bar', $message->getHeader('foo'));
-        $this->assertSame('bar', $message->getHeader('FOO'));
-        $this->assertSame('bar', $message->getHeader('fOO'));
+        $msg->setHeader('foo', 'bar');
+        $this->assertSame('bar', $msg->getHeader('foo'));
+        $this->assertSame('bar', $msg->getHeader('FOO'));
+        $this->assertSame('bar', $msg->getHeader('fOO'));
     }
 
     /**
      * Test that {@link HttpMessage::getHeaders()} preserves the case that was set.
      */
     public function testGetHeaders() {
-        $message = new HttpRequest();
+        $msg = new HttpRequest();
 
-        $message->setHeader('Foo', 'bar');
-        $headers = $message->getHeaders();
+        $msg->setHeader('Foo', 'bar');
+        $headers = $msg->getHeaders();
         $this->assertArrayHasKey('Foo', $headers);
         $this->assertArrayNotHasKey('foo', $headers);
-        $this->assertSame($message->getHEaderLines('foo'), $headers['Foo']);
+        $this->assertSame($msg->getHEaderLines('foo'), $headers['Foo']);
     }
 
     /**
      * Test headers with multiple values.
      */
     public function testMultipleHeaders() {
-        $message = new HttpRequest();
+        $msg = new HttpRequest();
 
-        $message
-            ->setHeader('foo', 'bar')
+        $msg->setHeader('foo', 'bar')
             ->addHeader('fOO', 'baz');
 
-        $this->assertSame('bar,baz', $message->getHeader('Foo'));
-        $this->assertSame(['bar', 'baz'], $message->getHeaderLines('FOO'));
+        $this->assertSame('bar,baz', $msg->getHeader('Foo'));
+        $this->assertSame(['bar', 'baz'], $msg->getHeaderLines('FOO'));
+    }
+
+    /**
+     * Test overwriting existing headers.
+     */
+    public function testHeaderOverwrite() {
+        $msg = new HttpRequest();
+
+        $msg->setHeader('foo', 'bar');
+        $this->assertSame('bar', $msg->getHeader('foo'));
+
+        $msg->setHeader('foo', 'baz');
+        $this->assertSame('baz', $msg->getHeader('foo'));
+
+        $msg->setHeader('foo', 'bar')
+            ->addHeader('fOO', 'baz')
+            ->setHeader('foo', 'world');
+        $this->assertSame('world', $msg->getHeader('foo'));
+    }
+
+    public function testHeaderRemove() {
+        $msg = new HttpRequest();
+
+        $msg->setHeader('foo', 'bar');
+        $this->assertTrue($msg->hasHeader('foo'));
+
+        $msg->setHeader('foo', null);
+        $this->assertFalse($msg->hasHeader('foo'));
     }
 }
