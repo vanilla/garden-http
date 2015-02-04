b6805bb3fa6a4595cb9d20feb8251139834c1dd6
Todd Burry <todd@vanillaforums.com>
2015-02-04 10:35:03 -0500

    Add hhvm support to our travis install.sh.

--- travis/install.sh
+++ travis/install.sh
@@ -10,19 +10,30 @@ sudo service nginx stop
 
 USER=$(whoami)
 DIR=$(realpath $(dirname "$0"))
+TRAVIS_PHP_VERSION=$(phpenv version-name)
 DOCUMENT_ROOT=$(realpath "$DIR/../tests")
-PHP_FPM_BIN="$HOME/.phpenv/versions/$TRAVIS_PHP_VERSION/sbin/php-fpm"
-PHP_FPM_CONF="$DIR/php-fpm.conf"
-PHP_FPM_LISTEN="$DIR/php-fpm.sock"
+SOCKET="$DIR/php-fpm.sock"
 
-# Build the php-fpm.conf.
-sudo sed -e "s|{USER}|$USER|g" -e "s|{PHP_FPM_LISTEN}|$PHP_FPM_LISTEN|g" < "$DIR/php-fpm.conf.tpl" > "$DIR/php-fpm.conf"
+if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ] || [ "$TRAVIS_PHP_VERSION" = 'hhvm_nightly' ]
+then
+    LOG_PATH="$DIR/hhvm.log"
+
+    sudo hhvm --mode=daemon --user="$USER" -vServer.Type=fastcgi -vLog.File="$LOG_PATH"
+else
+    PHP_FPM_BIN="$HOME/.phpenv/versions/$TRAVIS_PHP_VERSION/sbin/php-fpm"
+    PHP_FPM_CONF="$DIR/php-fpm.conf"
+
+    # Build the php-fpm.conf.
+    sudo sed -e "s|{USER}|$USER|g" -e "s|{PHP_FPM_LISTEN}|$SOCKET|g" < "$DIR/php-fpm.conf.tpl" > "$DIR/php-fpm.conf"
+
+    # Start php-fpm
+    sudo ${PHP_FPM_BIN} --fpm-config "$DIR/php-fpm.conf"
+fi
 
 # Build the default site nginx conf.
-sudo sed -e "s|{DOCUMENT_ROOT}|$DOCUMENT_ROOT|g" -e "s|{PHP_FPM_LISTEN}|$PHP_FPM_LISTEN|g" < "$DIR/default.conf.tpl" > "$DIR/default.conf"
+sudo sed -e "s|{DOCUMENT_ROOT}|$DOCUMENT_ROOT|g" -e "s|{PHP_FPM_LISTEN}|$SOCKET|g" < "$DIR/default.conf.tpl" > "$DIR/default.conf"
 sudo cp "$DIR/fastcgi.conf" /etc/nginx/fastcgi.conf
 sudo cp "$DIR/default.conf" /etc/nginx/sites-enabled/default.conf
 
-# Start the servers.
-sudo $PHP_FPM_BIN --fpm-config "$DIR/php-fpm.conf"
+# Start the nginx.
 sudo service nginx start
