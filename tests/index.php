112123fcf3d7b16cf1df7ac31b4ff698510e2e0f
Todd Burry <todd@vanillaforums.com>
2015-02-04 22:07:53 -0500

    Change the namespaces of the test server for easier test class creation.

--- tests/index.php
+++ tests/index.php
@@ -1,56 +1,57 @@
 <?php
-namespace Garden;
+namespace Garden\Http\Tests {
+    error_reporting(E_ALL); //E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);
+    ini_set('display_errors', '1');
+    ini_set('track_errors', '1');
 
-use Garden\Exception\ClientException;
+    // Define the root path of the application.
+    define('PATH_ROOT', __DIR__);
 
-error_reporting(E_ALL); //E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);
-ini_set('display_errors', '1');
-ini_set('track_errors', '1');
+    // Require the autoloader. You can also replace this with composer's autoload.php.
+    require_once __DIR__.'/../vendor/autoload.php';
+}
 
-// Define the root path of the application.
-define('PATH_ROOT', __DIR__);
+namespace Garden {
+    use Garden\Exception\ClientException;
 
-// Require the autoloader. You can also replace this with composer's autoload.php.
-//require_once __DIR__.'/autoload.php';
-require_once __DIR__.'/../vendor/autoload.php';
+    // Instantiate the application.
+    $app = new Application();
 
-// Instantiate the application.
-$app = new Application();
+    // Register routes.
+    $app->route('/hello.json', function () {
+        return [200, [], 'Hello world'];
+    });
 
-// Register routes.
-$app->route('/hello.json', function() {
-    return [200, [], 'Hello world'];
-});
+    $app->route('/echo(/.+)?\.json', function () use ($app) {
+        $request = $app->request;
 
-$app->route('/echo(/.+)?\.json', function() use ($app) {
-    $request = $app->request;
+        $result = [
+            'method' => $request->getMethod(),
+            'host' => $request->getHost(),
+            'path' => $request->getFullPath(),
+            'port' => $request->getPort(),
+            'headers' => $request->getHeaders(),
+            'query' => $request->getQuery(),
+            'body' => $request->getInput(),
+            'foo' => 'bar'
+        ];
 
-    $result = [
-        'method' => $request->getMethod(),
-        'host' => $request->getHost(),
-        'path' => $request->getFullPath(),
-        'port' => $request->getPort(),
-        'headers' => $request->getHeaders(),
-        'query' => $request->getQuery(),
-        'body' => $request->getInput(),
-        'foo' => 'bar'
-    ];
+        return $result;
+    });
 
-    return $result;
-});
+    $app->route('/basic-protected/{username}/{password}\.json', function ($username = '', $password = '') use ($app) {
+        $request = $app->request;
 
-$app->route('/basic-protected/{username}/{password}\.json', function($username = '', $password = '') use ($app) {
-    $request = $app->request;
+        if ($request->getEnv('PHP_AUTH_USER') !== $username) {
+            throw new ClientException('Invalid username.', 401, ['HTTP_WWW-Authenticate' => 'Basic realm="tests"']);
+        } elseif ($request->getEnv('PHP_AUTH_PW') !== $password) {
+            throw new ClientException('Invalid password.', 401, ['HTTP_WWW-Authenticate' => 'Basic realm="tests"']);
+        } else {
+            return ['message' => 'You are in.'];
+        }
+    });
 
-    if ($request->getEnv('PHP_AUTH_USER') !== $username) {
-        throw new ClientException('Invalid username.', 401, ['HTTP_WWW-Authenticate' => 'Basic realm="tests"']);
-    } elseif ($request->getEnv('PHP_AUTH_PW') !== $password) {
-        throw new ClientException('Invalid password.', 401, ['HTTP_WWW-Authenticate' => 'Basic realm="tests"']);
-    } else {
-        return ['message' => 'You are in.'];
-    }
-});
-
-// Run the application.
-$app->run();
+    // Run the application.
+    $app->run();
+}
 
