f986166aea6e098085195783f4ca955dd2af0307
Olivier Lamy-Canuel <olivier.lamy.canuel@gmail.com>
2024-11-20 15:02:24 -0500

    fix tests

--- tests/Fixtures/EchoHandler.php
+++ tests/Fixtures/EchoHandler.php
@@ -11,17 +11,24 @@ use Garden\Http\HttpHandlerInterface;
 use Garden\Http\HttpRequest;
 use Garden\Http\HttpResponse;
 
-class EchoHandler implements HttpHandlerInterface {
-    public function send(HttpRequest $request): HttpResponse {
-        parse_str(parse_url($request->getUrl(), PHP_URL_QUERY), $get);
+class EchoHandler implements HttpHandlerInterface
+{
+    public function send(HttpRequest $request): HttpResponse
+    {
+        $url = parse_url($request->getUrl(), PHP_URL_QUERY);
+        if ($url) {
+            parse_str($url, $get);
+        } else {
+            $get = [];
+        }
 
         $response = new HttpResponse();
         $response->setBody([
-            'method' => $request->getMethod(),
-            'url' => $request->getUrl(),
-            'headers' => $request->getHeaders(),
-            'get' => $get,
-            'body' => $request->getBody(),
+            "method" => $request->getMethod(),
+            "url" => $request->getUrl(),
+            "headers" => $request->getHeaders(),
+            "get" => $get,
+            "body" => $request->getBody(),
         ]);
 
         $response->setRequest($request);
